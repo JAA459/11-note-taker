c1d411c632d450603e1edd1fd5691d0403f5e539
Joshua Arroyo <Jalexarroyo@gmail.com>
2021-02-05 18:10:06 -0700

    front and back end linked.. and semi functioning delete button

--- Develop/db/server.js
+++ Develop/db/server.js
@@ -2,9 +2,14 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 var path = require("path");
+const bodyParser = require('body-parser');
+
 
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json());
+app.use(express.static("../public"));
+
 
 require("../routes/apiRoutes")(app);
 require("../routes/htmlRoutes")(app);
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -7,28 +7,13 @@ const crypto = require("crypto");
 function generateUniqueID() { 
     return crypto.randomBytes(8).toString('hex') 
   } 
-// var filepath = "../db/db.json"
-// var notes = fs.readFile(filepath, 'utf8', (error, data) =>
-// error ? console.error(error) : console.log(data)
-// );
 
 module.exports = function (app) {
-//     // API GET Requests
-//     // Below code handles when users "visit" a page.
-//     // In each of the below cases when a user visits a link
-//     // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-//     // ---------------------------------------------------------------------------
+
     app.get("/api/notes", function (req, res) {
         res.json(notes);
     });
   
-//     // API POST Requests
-//     // Below code handles when a user submits a form and thus submits data to the server.
-//     // In each of the below cases, when a user submits form data (a JSON object)
-//     // ...the JSON is pushed to the appropriate JavaScript array
-//     // (ex. User fills out a reservation request... this data is then sent to the server...
-//     // Then the server saves the data to the tableData array)
-//     // ---------------------------------------------------------------------------
 app.post("/api/notes", function (req, res) {
     const file = path.join(__dirname,"../db/db.json");
     const newNote = req.body;
@@ -43,30 +28,21 @@ app.post("/api/notes", function (req, res) {
     });
     res.send(newNote);
 });
-//     // app.post("/api/notes", function (req, res) {
-//     //   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-//     //   // It will do this by sending out the value "true" have a table
-//     //   // req.body is available since we're using the body parsing middleware
-//     //   if (tableData.length < 5) {
-//     //     tableData.push(req.body);
-//     //     res.json(true);
-//     //   }
-//     //   else {
-//     //     waitListData.push(req.body);
-//     //     res.json(false);
-//     //   }
-//     // });
+app.delete("/api/notes/:id", (req, res) => {
+    const id = req.param.id;
+    const file = path.join(__dirname, "../db/db.json");
   
-//     // ---------------------------------------------------------------------------
-//     // I added this below code so you could clear out the table while working with the functionality.
-//     // Don"t worry about it!
-  
-//     // app.post("/api/clear", function (req, res) {
-//     //   // Empty out the arrays of data
-//     //   tableData.length = 0;
-//     //   waitListData.length = 0;
-  
-//     //   res.json({ ok: true });
-//     // });
+    for(const note of notes) {
+      if (id === notes.id) {
+        const index = notes.indexOf(note);
+        notes.splice(index, 1);
+        fs.writeFile(file, JSON.stringify(notes, null, 4), err => {
+          if (err) throw err;
+          console.log("Your note was deleted.");
+        })
+        res.end();
+      }
+    }});
+
   };
   
